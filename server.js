e9116634d7eb3853455fdf57a508961b5ff591f3
ddohing <dnflwlq004@naver.com>
2022-04-18 17:22:19 +0900

    Mysql 연결 및 Customer DB 구축

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
  * 명령어를 통해 포트 사용 여부도 확인 가능
  * */
 
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -13,33 +14,23 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect();
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "박서함",
-      birthday: "931028",
-      gender: "남자",
-      job: "배우",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "박재찬",
-      birthday: "011206",
-      gender: "남자",
-      job: "아이돌",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "최소희",
-      birthday: "000108",
-      gender: "여자",
-      job: "프론트엔드 개발자",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => {
