6d2a4137be8c1bf4d3d62383fbd3f5a22bff0d65
ddohing <dnflwlq004@naver.com>
2022-04-18 22:51:38 +0900

    Materual Modal 디자인 구현

--- client/src/component/CustomerAdd.js
+++ client/src/component/CustomerAdd.js
@@ -1,5 +1,20 @@
 import React, { useState } from "react";
 import { post } from "axios";
+import {
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  Button,
+  withStyles,
+} from "@material-ui/core";
+
+const styles = (theme) => ({
+  hidden: {
+    display: "none",
+  },
+});
 
 function CustomerAdd(props) {
   const [file, setFile] = useState(null);
@@ -11,6 +26,7 @@ function CustomerAdd(props) {
     job: "",
   });
   const { username, birthday, gender, job } = inputs;
+  const [open, setOpen] = useState(false);
 
   const handleFormSubmit = (e) => {
     e.preventDefault(); // 데이터가 서버로 전달됨으로써 오류가 발생하지 않도록 함
@@ -21,6 +37,7 @@ function CustomerAdd(props) {
     setFile(null);
     setFileName("");
     setInputs({ username: "", birthday: "", gender: "", job: "" });
+    setOpen(false);
   };
 
   const addCustomer = () => {
@@ -54,48 +71,95 @@ function CustomerAdd(props) {
     setInputs(nextInputs);
   };
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setFile(null);
+    setFileName("");
+    setInputs({ username: "", birthday: "", gender: "", job: "" });
+    setOpen(false);
+  };
+
+  const { classes } = props;
+
   return (
-    <form onSubmit={handleFormSubmit}>
-      <h1>고객 추가</h1>
-      프로필 이미지:
-      <input
-        type="file"
-        name="file"
-        file={file}
-        value={fileName}
-        onChange={handleFileChange}
-      />
-      <br />
-      이름:
-      <input
-        type="text"
-        name="username"
-        value={username}
-        onChange={handleValueChange}
-      />
-      <br />
-      생년월일:
-      <input
-        type="text"
-        name="birthday"
-        value={birthday}
-        onChange={handleValueChange}
-      />
-      <br />
-      성별:
-      <input
-        type="text"
-        name="gender"
-        value={gender}
-        onChange={handleValueChange}
-      />
-      <br />
-      직업:
-      <input type="text" name="job" value={job} onChange={handleValueChange} />
-      <br />
-      <button type="submit">추가하기</button>
-    </form>
+    <div>
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        고객 추가하기
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>고객 추가</DialogTitle>
+        <DialogContent>
+          <input
+            className={classes.hidden}
+            accept="image/*"
+            id="raised-button-file"
+            type="file"
+            file={file}
+            value={fileName}
+            onChange={handleFileChange}
+          />
+          <label htmlFor="raised-button-file">
+            <Button
+              variant="contained"
+              color="primary"
+              component="span"
+              name="file"
+            >
+              {fileName === "" ? "프로필 이미지 선택" : fileName}
+            </Button>
+          </label>
+          <br />
+          <TextField
+            label="이름"
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleValueChange}
+          />
+          <br />
+          <TextField
+            label="생년월일"
+            type="text"
+            name="birthday"
+            value={birthday}
+            onChange={handleValueChange}
+          />
+          <br />
+          <TextField
+            label="성별"
+            type="text"
+            name="gender"
+            value={gender}
+            onChange={handleValueChange}
+          />
+          <br />
+          <TextField
+            label="직업"
+            type="text"
+            name="job"
+            value={job}
+            onChange={handleValueChange}
+          />
+          <br />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleFormSubmit}
+          >
+            추가
+          </Button>
+          <Button variant="outlined" color="primary" onClick={handleClose}>
+            닫기
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
--- client/src/component/CustomerDelete.js
+++ client/src/component/CustomerDelete.js
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
 function CustomerDelete(props) {
+  const [open, setOpen] = useState(false);
+
   const deleteCustomer = (id) => {
     const url = "/api/customers/" + id;
     fetch(url, {
       method: "DELETE",
-    }).then(props.stateRefresh());
+    });
+    setOpen(false);
+    // }).then(props.stateRefresh());
     // stateRefresh()가 동작하지 않는 오류 발생
   };
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <button
-      onClick={(e) => {
-        deleteCustomer(props.id);
-      }}
-    >
-      삭제
-    </button>
+    <div>
+      <Button variant="contained" color="secondary" onClick={handleClickOpen}>
+        삭제
+      </Button>
+      <Dialog onClose={handleClose} open={open}>
+        <DialogTitle onClose={handleClose}>삭제 경고</DialogTitle>
+        <DialogContent gutterBottom>
+          <Typography>선택한 고객 정보가 삭제됩니다.</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={(e) => {
+              deleteCustomer(props.id);
+            }}
+          >
+            삭제
+          </Button>
+          <Button variant="outlined" color="secondary" onClick={handleClose}>
+            닫기
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 }
 
